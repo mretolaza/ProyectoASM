05a27ad82fe0e9b3b7eaf18b1ae1d2ec474c46b4
ret16339 <ret16339@uvg.edu.gt>
2017-05-22 23:25:28 -0600

    Cambio de gpios

--- Programa/asm/set_buttons.s
+++ Programa/asm/set_buttons.s
@@ -13,34 +13,35 @@
 @@      cuales pines seran de escritura y letura para que se puedan usar las funciones creadas en readGPIOS.s
 
 .global set_buttons
+
 set_buttons:
 push {lr}
     @ Generar direccion de memoria virtual para alterar GPIO
     bl GetGpioAddress
 
     @ Configurar GPIO's para lectura de botones frontales
-    mov r0, #17         @ GPIO 1 (Boton 1)
+    mov r0, #4        @ GPIO 1 (Boton 1; Minutos)
     mov r1, #0          @ Read
     bl SetGpioFunction
 
-    mov r0, #27         @ GPIO 2 (Boton 2)
+    mov r0, #17         @ GPIO 2 (Boton 2; Segundos)
     mov r1, #0          @ Read
     bl SetGpioFunction
 
-    mov r0, #22         @ GPIO 3 (Boton 3)
+    mov r0, #27         @ GPIO 3 (Boton 3; Seteo de alarma)
     mov r1, #0          @ Read
     bl SetGpioFunction
 
     @ Configurar GPIO's para escritura de los LED's
 
-    mov r0, #21         @ GPIO Led 1 
+    mov r0, #22         @ GPIO Led 1 
     mov r1, #1          @ Write
     bl SetGpioFunction
 
 
     @ Colocar todos los LED's en apagado
       
-        mov r0, #21         @ GPIO 1
+        mov r0, #22         @ GPIO 1
         mov r1, #0          @ Off
         bl SetGpio
 
