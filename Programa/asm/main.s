9b822a7ef1b3ed829c8133af076b61149833b2c4
ret16339 <ret16339@uvg.edu.gt>
2017-05-24 10:19:55 -0600

    Cambios

---
+++ Programa/asm/main.s
@@ -0,0 +1,11 @@
+
+@ -----------------------------------
+@ Universidad del Valle de Guatemala
+@ Organizacion de Computadoras y Assembler 
+@ Autor: Maria Mercedes Retolaza Reyna, 16339 
+@ Autor: Diego Fernandez,16344  
+@ ----------------------------------
+@ En esta sección del programa se programarán las entradas de los botones, la division de los numeros 
+@ las entradas que se utilizaran 
+@ Los requisitos para que esto se cumpla es establecer los GpioS para el funcionamiento previo 
+@ -----------------------------------
